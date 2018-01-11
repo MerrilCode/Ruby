3ab2d9255b28bf9006b3cfaf2fef62300ffde65d
Tech-A67 <tech-a67@Tech-A67s-MacBook-Air.local>
2018-01-11 16:58:23 +0000

    added the view engine

--- sinatra-intro/config.ru
+++ sinatra-intro/config.ru
@@ -2,4 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative './controllers/contacts_controller'
 
+use Rack::Reloader
+
+use Rack::MethodOverride
+
 run ContactsController
--- sinatra-intro/controllers/contacts_controller.rb
+++ sinatra-intro/controllers/contacts_controller.rb
@@ -1,37 +1,105 @@
 class ContactsController < Sinatra::Base
+ # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+  
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+  
+  configure :development do
+      register Sinatra::Reloader
+  end
+
+	$contacts = [{
+			id:0,
+			name:"merril",
+			email: "mmathew@spartaglobal.com"
+		},
+		{
+			id: 1,
+			name: "Markson Aigbodi",
+			email: "maigbody@spartaglobal.com"
+		},
+		{
+			id: 2,
+			name: "Jack McGregor",
+			email: "jmcgregor@spartaglobal.com"
+		}
+	]
 
 #INDEX
 
-get '/contacts' do
-  "INDEX"
-end
-# SHOW
-get '/contacts/:id' do
-	id = params[:id]
-	"show: #{id}"
-end
+	get '/contacts' do
+		@title = "Contacts"
+		@contacts = $contacts
+	  	erb :"contacts/index"
+	end
 
-# POST
+	get '/contacts/new' do
+		@name = "Name"
+		@email = "Email:"
+		erb :"contacts/new"
+	end
 
-post '/contacts/post' do
-	"POST"
-end
+	# EDIT
 
-# NEW
-get '/contacts/new' do
-	"new"
-end
+	get '/contacts/:id/edit' do
+		id = params[:id]
+		@contact = $contacts[id.to_i]
+		erb :"contacts/edit"
+	end
 
-# UPADATE
+	#UPDATE
+    put '/contacts/:id' do
+        id = params[:id].to_i
+        # contact = $contacts[id]
+        # # id = params[:id]
+      
+        # contact[params[:name]] = params[:name]
+        # contact[params[:email]] = params[:email]
+        # puts params
+        edited_contact = {
+        	id: id,
+        	name: params[:name],
+        	email: params[:email]
+        }
+        $contacts[id] = edited_contact
+        redirect "/contacts/#{id}"
 
-put '/contacts/update' do
-	"update"
-end
-#DELETE
+       
+    end
 
-delete'/contacts/:id' do
-	id = params[:id]
-	"DELETE: #{id}"
-end
+	# NEW
+	get '/contacts/test' do
+		erb :"contacts/test"
+	end
+
+	# SHOW
+	get '/contacts/:id' do
+		id = params[:id]
+		@contact = $contacts[id.to_i]
+		erb :"contacts/show"
+		# "show: #{id}"
+	end
+
+	# POST
+
+	post '/contacts' do
+		new_contact = {
+			id: $contacts.length,
+			name: params[:name],
+			email: params[:email]
+		}
+		$contacts.push(new_contact)
+		redirect "/contacts"
+	end
+
+
+	#DELETE
+
+	delete '/contacts/:id' do
+		id = params[:id].to_i
+		$contacts.delete_at(id)
+		redirect '/contacts'
+	end
 	
 end
